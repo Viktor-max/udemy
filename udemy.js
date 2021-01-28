aff7f1475ecdcc18c2a914b7a9053093f059967d
Viktor <viktorstankevich@inbox.ru>
2021-01-29 00:57:22 +0300

    first commit

---
+++ udemy.js
@@ -0,0 +1,116 @@
+"use strict";
+
+// let number = 5;
+// const leftBorderWidth = 1;
+// number = 10;
+// console.log(number);
+
+// const obj = {
+//     a: 50
+// };
+
+// obj.a = 10;
+
+// console.log(obj);  //Прямых констант в JS не бывает
+
+// {
+//     let result = 50;
+// }
+
+// console.log(result);
+
+// _______________________________________________________________________
+
+// Типы данных
+
+// let number = 4.6;
+// console.log(4/0);
+// console.log('string'*9);
+
+// const person = "Alex";
+
+// const bool = true;
+
+// // console.log(something);
+
+// let und;
+// console.log(und);
+
+
+// const obj = {
+//     name: "John",
+//     age: 25,
+//     isMarried: false
+// };
+
+// // console.log(obj.name);
+
+//  const arr = ['belarus.jpg', 5, {}, []];
+
+//  ____________________________________________________________________
+
+// Общение с пользователем
+
+// alert('Hello');
+
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// const answer = +prompt("Вам есть 18?", "18");
+// console.log(answer + 5);
+
+
+// const answers = [];
+
+// // answers[0] = prompt("Как ваше имя?", "");
+// // answers[1] = prompt("Как ваша фамилия?", "");
+// // answers[2] = prompt("Сколько вам лет?", "");
+
+// console.log(typeof(answers));
+// console.log(typeof(null));
+// ____________________________________________________________________
+
+// Интерполяция
+
+// const category = "toys";
+
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = "Ivan";
+
+// alert(`Привет, ${user}`);
+// ____________________________________________________________________
+
+// Операторы JS
+
+// console.log(4 + +'5');
+
+//  let incr = 10;
+//  let decr = 10;
+
+//  ++incr;
+//  --decr;
+
+//  console.log(++incr);
+//  console.log(--decr);
+
+// Остаток от деления
+//  const remainder = 150 % 12;
+//  console.log(remainder);
+
+//  &&(и) и ||(или)
+//  const isChecked = false;
+//  const isClose = false;
+ 
+//  console.log(isChecked || isClose);
+
+//  !(отрицание)
+
+// const isChecked = false;
+// const isClose = false;
+ 
+// console.log(isChecked || !isClose);
+
+// console.log(2 + 2 * 2 == '6');
+ 
+
